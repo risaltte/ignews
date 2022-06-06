eb26cdb325ddbb55be3ce1c0f26662b0fac4247f
risaltte <risaltte@gmail.com>
2022-06-06 16:36:49 -0400

    Login OAuth Github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
+import { SessionProvider as NextAuthPovider } from 'next-auth/react'
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthPovider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthPovider>
   );
 }
 
