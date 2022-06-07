99bcc6d695be66d7341b04305ace34bcae44b154
risaltte <risaltte@gmail.com>
2022-06-07 17:18:03 -0400

    Inicio callback signIn

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 
+import { query as q } from 'faunadb'
+
+import { fauna } from "../../../services/fauna";
+
 export default NextAuth({
     providers: [
         GithubProvider({
@@ -11,9 +15,24 @@ export default NextAuth({
                     scope: "read:user"
                 }
             }
-        }),
+        }),       
 
         // ...add more providers here
     ],
+
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            const { email: emailAdress  } = user;
+
+            await fauna.query(
+                q.Create(
+                    q.Collection('users'),
+                    { data: { email: emailAdress } }
+                )
+            );
+
+            return true;
+        },
+    }
     
 });
