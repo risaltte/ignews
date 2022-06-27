5b7fdd290e7a437d4f2abecb4afc835052ad1dc6
risaltte <risaltte@gmail.com>
2022-06-27 16:51:01 -0400

    Gerando previews estáticos

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
@@ -55,9 +55,12 @@ export default function PostPreview({ post }: PostPreviewProps) {
     );
 }
 
-export const getStaticPaths = () => {
+// getStaticPaths só existe em páginas dinamicas que possuem parâmetros 
+export const getStaticPaths: GetStaticPaths = async () => {
     return {
-        paths: [],
+        paths: [
+            // { params: { slug: 'obtendo-o-status-de-progresso-do-envio-de-dados-com'} }
+        ],
         fallback: 'blocking'
     }
 }
@@ -87,6 +90,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     return {
         props: {
             post
-        }
+        },
+        revalidate: 60 * 30, // 30 minutos
     }
 }
