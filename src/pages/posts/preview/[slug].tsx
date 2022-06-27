90eaa7928908f10ac5728f0253a627d68a78afff
risaltte <risaltte@gmail.com>
2022-06-27 16:31:22 -0400

    Página preview do post

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,13 +1,15 @@
-import { GetServerSideProps } from "next";
-import { getSession } from "next-auth/react";
-import { redirect } from "next/dist/server/api-utils";
+import { GetStaticProps } from "next";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { RichText } from "prismic-dom";
-import { getPrismicClient } from "../../services/prismic";
+import { useEffect } from "react";
+import { getPrismicClient } from "../../../services/prismic";
 
-import styles from "./post.module.scss";
+import styles from "../post.module.scss";
 
-interface PostProps {
+interface PostPreviewProps {
     post: {
         slug: string;
         title: string;
@@ -16,7 +18,16 @@ interface PostProps {
     }
 }
 
-export default function Post({ post }: PostProps) {
+export default function PostPreview({ post }: PostPreviewProps) {
+    const { data:session } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (session?.activeSubscription) {
+            router.push(`/posts/${post.slug}`);
+        }
+    }, [session]);
+
     return (
         <>
             <Head>
@@ -28,35 +39,36 @@ export default function Post({ post }: PostProps) {
                     <h1>{post.title}</h1>
                     <time>{post.updatedAt}</time>
                     <div 
-                        className={styles.postContent}
+                        className={`${styles.postContent} ${styles.previewContent}`}
                         dangerouslySetInnerHTML={{__html: post.content}}
                     />
+
+                    <div className={styles.continueReading}>
+                        Wanna continue reading?
+                        <Link href={"/"}>
+                            <a>Subscribe now 🤗</a>
+                        </Link>
+                    </div>
                 </article>
             </main>
         </>
     );
 }
 
+export const getStaticPaths = () => {
+    return {
+        paths: [],
+        fallback: 'blocking'
+    }
+}
+
 // Foi escolhido dinamico  - SSR - porque o Statico - SSG não tem como tornar o post privado. 
 // Para er um post o usuário precisa estar logado e ser assinante. 
 //Por padrao no SSG não tem restrição e as informações são publicas. 
-export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-    const session = await getSession({ req });
-
-    // Se não tiver uma inscrição ativa redireciona
-    if (!session?.activeSubscription) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false
-            }
-        }
-    }
-
+export const getStaticProps: GetStaticProps = async ({ params }) => {
     const { slug } = params;
 
-    // Instanciar client prismic passando a requisição
-    const prismic = getPrismicClient(req);
+    const prismic = getPrismicClient();
 
     // Buscar post pelo parametro slug
     const response = await prismic.getByUID('post', String(slug), {});
@@ -64,7 +76,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     const post = {
         slug,
         title: response.data.title,
-        content: RichText.asHtml(response.data.content),
+        content: RichText.asHtml(response.data.content.splice(0, 3)), // vai pegar apenas os 3 primeiros itens
         updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
             day: '2-digit',
             month: 'long',
